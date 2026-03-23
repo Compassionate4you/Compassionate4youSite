2d1547ac71fb9635e71378aec15ba6ce5e6f9e63
Mamon Poian <Mamonpoian02@gmail.com>
2026-03-22 18:40:38 -0700

    Added home health page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import ChatbotWidget from './components/chatbot/app_imports/ChatbotWidget';
+import HomeHealthPage from "./pages/HomeHealthPage";
 //import AccessibilityPanel from './components/accessibility/AccessibilityPanel';
 
 function App() {
     return (
         <div className="app">
             <ChatbotWidget/>
+            <HomeHealthPage/>
         </div>
     );
 }
