95772f0949eb16f1eddb0a718fb6507ab1ac6f78
TuboLax <159499044+TuboLax@users.noreply.github.com>
2026-04-11 18:09:11 -0700

    Added Hospice Page. Styling and translations are included. Task #DT-31

---
+++ client/src/pages/HospicePage.jsx
@@ -0,0 +1,244 @@
+import React from "react";
+
+function HospicePage(){
+    return(
+        <main>
+            <section class="HospiceBanner">
+                <div class="HospiceBannerMiddle">
+                    <div class="HospiceBlurb">
+                        <div class="HospiceTitle">
+                            <div class="HospiceIcon">
+                                <svg class="HospiceHeart">
+
+                                </svg>
+                            </div>
+                            <h1 class="HospiceTitleText">Hospice</h1>
+                        </div>
+                        <p class="HospiceDescription">
+                            Providing comfort, dignity, and compassionate support during life's final journey. We're here for patients and their families every step of the way.
+                        </p>
+                        <a class="HospiceContactButton">
+                            Contact Our Team
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <section class="UnderstandingHospiceSection">
+                <h2 class="UnderstandingHospiceTitle">Understanding Hospice Care</h2>
+                <div class="HospiceInformationCards">
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">What is Hospice</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Hospice care focuses on providing comfort, quality of life, and support for people nearing the end of life, rather than curing the illness. It is provided by a team of professionals who manage pain and other symptoms, and address emotional, physical, spiritual, and social needs of both the patient and their family through passion. Hospice care can be provided in a person's home, a dedicated hospice facility, and the goal is to help the person live their remaining time with dignity and as peacefully as possible.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Who is eligible?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                No matter your faith or beliefs, you and your family can receive spiritual and emotional support from our care team.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">How is hospice different?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Our team provides a compassionate environment for patients and families, so they can feel supported and understood during end-of-life care
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Why might I choose?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Our hospice team provides emotional and spiritual support to families as they navigate end-of-life care.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Who provides the care?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                The foundation of our service is a team of highly skilled and trained professionals providing compassionate and personalized care tailored to the unique needs of each individual. We have a disciplinary team of various professionals, including physicians, nurses, social workers, chaplains, and hospice aides, to provide comprehensive care and support to patients and their families. Other disciplines like physical, occupational, and speech therapists, as well as volunteers and bereavement specialists, may also be involved, depending on the patient's individualized specific needs and the hospice provider's orders.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+            <section class="HospiceCare&Benefits">
+                <h2 class="HospiceCare&BenefitsTitle">Hospice Care Team & Benefits</h2>
+                <div class="HospiceInformationCards">
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">What does hospice provide?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Hospice offers care from a team of different types of practioners who work together to meet the medical, psychological and spiritual needs of you and your family.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Hours avail when in hospice care...</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Hospice provides you a phone number to call 24 hours a day, 7 days a week, to help answer any questions you may have.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">What other benefits do patients have when they are under hospicecare</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Once you are admitted into the Hospice Program, you will be provided with:
+                            </p>
+                            <ul class="HospiceCardList">
+                                <li class="HospiceCardListItem">
+                                    medical appliances
+                                </li>
+                                <li class="HospiceCardListItem">
+                                    medical supplies
+                                </li>
+                                <li class="HospiceCardListItem">
+                                    medications for symptoms and pain relief
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Physician</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Your personal physician continues to provide your care while you're in the Hospice Program. A hospice physician works closely with your personal physician and can consult with you.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Nurse</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                A nurse visits during the week to instruct your caregiver and to coordinate your care. A nurse informs the hospice physician of your condition on a regular basis.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Social worker</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                A social worker helps you and your caregiver by:
+                            </p>
+                            <ul class="HospiceCardList">
+                                <li class="HospiceCardListItem">
+                                    offering emotional support
+                                </li>
+                                <li class="HospiceCardListItem">
+                                    assisting with practical matters, such as finding additional help in the home
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="HospiceComprehensiveServices">
+                <h2 class="HospiceComprehensiveServicesTitle">Comprehensive Support Services</h2>
+                <div class="HospiceInformationCards">
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Hospiceaide/homemaker services</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                A hospice aide visits during the week as needed to help you and your caregiver with personal care such as changing linens, giving you a bath and helping you get dressed.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Chaplain</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                A chaplain is a minister or counselor dedicated to meeting the spiritual needs of you and your family, no matter your religion. You and your family can ask for the chaplain anytime.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">Volunteer</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                A volunteer is someone from the community who has been trained by our Hospice Program to help you and your family. Volunteers provide services under the direction of a hospice staff member.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">What about medical appointments?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                Your personal physician is in charge of your care, even after you're admitted to our Hospice Program. A hospice physician will work with your personal physician to provide the care your need.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">What kind of help might I need?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                At some point, you may become confined to bed and unable to turn over or eat without help. Your caregiver may need to change your diapers and linens and care for your skin to prevent bedsores. Hospice nurses will teach caregivers how to perform these tasks.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="HospiceInformationCard">
+                        <div class="HospiceInformationCardTop">
+                            <h3 class="HospiceInformationCardTitle">What if a difficult situation arises?</h3>
+                        </div>
+                        <div class="HospiceInformationCardBottom">
+                            <p class="HospiceInformationCardText">
+                                We understand that caregivers may face difficult situations. A 24-hour phone line puts the caregiver in touch with a hospice-trained nurse who can give advice over the phone or make a home visit if necessary.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="HospiceAbove&Beyond">
+
+            </section>
+            <section class="HospiceProcess">
+
+            </section>
+            <section class="HospiceContact">
+
+            </section>
+        </main>
+    );
+}
+
+export default HospicePage;
