9ea5da18d01a4583461066388fe957d5a5ac6247
RedBlueBox <165118340+RedBlueBox@users.noreply.github.com>
2026-04-11 19:44:59 -0700

    Update ClientSchedulingConfirmation.jsx
    changed to import scheduling.css

--- client/src/pages/ClientSchedulingConfirmation.jsx
+++ client/src/pages/ClientSchedulingConfirmation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/portal.css";
+import "../styles/scheduling.css";
 
 export function ClientSchedulingConfirmation({ appointment, onReset }) {
   return (
